8ba6b2eb4c1694f6a8250d510bf2d04698678a38
Pavel <isessed@gmail.com>
2023-09-10 16:03:16 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,30 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String NEW_VALUE = "";
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void letters(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    void crashed() {
+
+        String name;
+    }
+}
