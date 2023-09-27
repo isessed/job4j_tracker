dc0536ddba307ad8b0d325d24403a8b9ae7af9d1
Pavel <isessed@gmail.com>
2023-09-27 17:48:29 +0300

    1. Что такое полиморфизм. [#3819 #448229]

---
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -0,0 +1,22 @@
+package ru.job4j.poly;
+
+public class FileStore implements Store {
+
+    private String path;
+
+    public FileStore() {
+    }
+
+    public FileStore(String path) {
+        this.path = path;
+    }
+
+    public void save(String data) {
+         //Тут реализация хранения данных в файле
+    }
+
+    public String read() {
+         // Тут чтение файла
+        return null;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Service.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Service {
+
+    private Store store;
+
+    public Service(Store store) {
+        this.store = store;
+    }
+
+    public void add() {
+        store.save("Pavel Petrov");
+    }
+
+    public static void main(String[] args) {
+        FileStore store = new FileStore();
+        Service service = new Service(store);
+        service.add();
+    }
+}
