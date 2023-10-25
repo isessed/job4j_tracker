d4bba956af73c47210031aa6162a0ff99d619d0e
isess <isessed@gmaIL.com>
2023-10-25 17:49:03 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String words : origin) {
+            check.add(words);
+        }
+        for (String words : text) {
+            if (!check.contains(words)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
