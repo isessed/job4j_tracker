334f55014f14096ad003ca5625c3d8981e9a8885
isess <isessed@gmaIL.com>
2023-12-11 15:21:16 +0300

    0. Map, HashMap [#212652 #465271]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("isessed@gmail.com", "Петров Павел Валентинович");
+        map.put("parsentev@yandex.ru", "Петр Арсентьев");
+        map.put("123@gmail.com", "Иван Иванов");
+        map.put("123@gmail.com", "Иван Сидоров");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
