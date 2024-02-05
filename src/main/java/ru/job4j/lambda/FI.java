8a178c3a2769774410aabbe3f420160b7091180c
isess <isessed@gmaIL.com>
2024-02-05 18:16:37 +0300

    2.1. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,20 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+
+    public static void main(String[] args) {
+        Attachment[] attachments = {
+                new Attachment("image 1", 20),
+                new Attachment("image 3", 120),
+                new Attachment("image 2", 23)
+        };
+        Comparator<Attachment> comparator = (left, right) -> Integer.compare(left.getSize(), right.getSize());
+        Comparator<String> comparatorSize = (left, right) -> Integer.compare(left.length(), right.length());
+        Arrays.sort(attachments, comparator);
+        Comparator<String> comparatorText = (left, right) -> left.compareTo(right);
+        Comparator<String> comparatorDescSize = (left, right) -> Integer.compare(right.length(), left.length());
+    }
+}
