5b042d278e50da96cf199ac6ab462bd64a2fe142
isess <isessed@gmaIL.com>
2024-02-25 18:13:11 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209 #483033]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -9,13 +9,6 @@ public class SearchFolder {
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
         List<Folder> result = new ArrayList<>();
         for (Folder folder : list) {
-            predicate = filterName -> folder.getName().contains("bug");
-            if (predicate.test(folder)) {
-                result.add(folder);
-            }
-        }
-        for (Folder folder : list) {
-            predicate = filterSize -> folder.getSize() > 100;
             if (predicate.test(folder)) {
                 result.add(folder);
             }
