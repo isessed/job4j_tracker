893a3d26d27d9129b104df2e2e71eb561128d0c9
isess <isessed@gmaIL.com>
2024-03-08 17:45:17 +0300

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/api/DropWhile.java
@@ -0,0 +1,14 @@
+package ru.job4j.api;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DropWhile {
+
+    public static void main(String[] args) {
+        List.of(1, 2, 3, 4, 1, 2).stream()
+                .dropWhile(value -> value < 3)
+                .map(value -> "Результат: " + value)
+                .forEach(System.out::println);
+    }
+}
